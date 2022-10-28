f8d8eced38d9dac4764c7cac712a36e27820ba84
simplylogicninjas <98150008+simplylogicninjas@users.noreply.github.com>
2022-10-28 09:03:05 +0200

    Modified snapTargets calculation

--- src/modules/interact.ts
+++ src/modules/interact.ts
@@ -2,10 +2,7 @@ import { Interactable, SnapTarget } from "@interactjs/types";
 import interact from "interactjs";
 import { Item } from "src/components/CanvasInteract";
 
-const AXIS_RANGE = 6;
-const CORNER_RANGE = 14;
-const CORNER_EXCLUDE_AXIS = 8;
-const AXIS_EXTRA_RANGE = -6;
+const AXIS_RANGE = 20;
 
 export class InteractModule {
     interactable: Interactable | undefined = undefined;
@@ -33,9 +30,9 @@ export class InteractModule {
             inertia: true,
             modifiers: [
                 interact.modifiers.snap({
-                    targets: (() => [...this.snapTargets])(),
+                    targets: this.snapTargets,
                     relativePoints: [
-                        { x: 0  , y: 0   }
+                        {x: 0, y: 0}
                     ]
                 })
             ],
@@ -44,13 +41,7 @@ export class InteractModule {
                     const target = event.target as HTMLElement;
 
                     this.currentElement = target;
-
-                    const pos = this.getPosition(target);
-
-                    this.offX1 = event.clientX - pos.x;
-                    this.offY1 = event.clientY - pos.y;
-                    this.offX2 = event.clientX - this.currentElement.offsetWidth - pos.x;
-                    this.offY2 = event.clientY - this.currentElement.offsetHeight - pos.y;
+                    this.getSnapTargets(target);
                 },
                 move: this.onDragMove.bind(this),
                 end: (event: any) => {
@@ -75,100 +66,68 @@ export class InteractModule {
 
     syncItems(items: Item[]) {
         this.items = [...items];
-        
-        this.getSnapTargets();
 
-        if (this.interactable) {
-            this.interactable.options.drag.modifiers = [
-                interact.modifiers.snap({
-                    targets: this.snapTargets
-                })
-            ]
-        }
+        // if (this.interactable) {
+        //     this.interactable.options.drag.modifiers = [
+        //         interact.modifiers.snap({
+        //             targets: this.snapTargets
+        //         })
+        //     ]
+        // }
     }
 
-    private getSnapTargets() {
-        this.snapTargets = [];
+    private getSnapTargets(target: HTMLElement) {
+        const id = target.getAttribute('data-id');
 
-        console.log(AXIS_EXTRA_RANGE, CORNER_RANGE, this.getSnapCoords);
+        if (id) {
+            const currentItem = this.items.find(it => it.id === id);
 
-        this.snapTargets.push(
-            () => {
-                return {
-                    y: 100,
-                    range: AXIS_RANGE
-                }
-            }
-        )
+            if (currentItem) {
+                const otherItems = this.items.filter(it => it.id !== id);
 
-        this.items.forEach(item => {
-            const element = document.getElementById(`canvas-item-${item.id}`);
+                const {width, height} = currentItem;
 
-            if (element) {
-                // Slide along the X axis
-                // this.snapTargets.push(
-                //     () => {
-                //         return {
-                //             x: item.x,
-                //             y: item.y,
-                //             range: AXIS_RANGE
-                //         }
-                //     }
-                // )
+                const halfWidth = width / 2;
+                const halfHeight = height / 2;
 
-                this.snapTargets.push(
-                    () => {
-                        return {
-                            x: item.x,
+                otherItems.forEach(item => {
+                    const itemX = item.width / 2;
+                    const itemY = item.height / 2;
+
+                    this.snapTargets = [
+                        {
+                            x: itemX,
                             range: AXIS_RANGE
-                        }
-                    }
-                )
-
-                this.snapTargets.push(
-                    () => {
-                        return {
-                            y: item.y,
+                        },
+                        {
+                            x: itemX - halfWidth,
                             range: AXIS_RANGE
+                        },
+                        {
+                            x: itemX - width,
+                            range: AXIS_RANGE
+                        },
+                        {
+                            y: itemY,
+                            range: AXIS_RANGE
+                        },
+                        {
+                            y: itemY - halfHeight,
+                            range: AXIS_RANGE
+                        },
+                        {
+                            y: itemY - height,
+                            range: AXIS_RANGE
+                        },
+                        {
+                            x: itemX,
+                            y: itemY,
+                            range: AXIS_RANGE * 2
                         }
-                    }
-                )
-                    /*
-                    () => {
-                    const data = this.getSnapCoords(element, "x");
-                    if (data.isOK) {
-                        return {
-                        x: data.x,
-                        range: AXIS_RANGE
-                        };
-                    }
-                    });
-                // Slide along the Y axis
-                this.snapTargets.push(
-                    () => {
-                    const data = this.getSnapCoords(element, "y");
-                    if (data.isOK) {
-                        return {
-                        y: data.y,
-                        range: AXIS_RANGE
-                        };
-                    }
-                    });
-                // Snap to corner
-                this.snapTargets.push(
-                    () => {
-                    const data = this.getSnapCoords(element);
-                    if (data.isOK) {
-                        return {
-                        x: data.x,
-                        y: data.y,
-                        range: CORNER_RANGE
-                        };
-                    }
-                    
-                    })*/;
+                    ]
+                })
             }
-        });
+        }
     }
 
     private getResizableConfig() {
@@ -286,93 +245,4 @@ export class InteractModule {
     
         return angle - startAngle;
     }
-
-    private getPosition(element: HTMLElement): {x: number, y: number} {
-        return {
-          x: parseFloat(element.getAttribute('data-x') as string) || 0,
-          y: parseFloat(element.getAttribute('data-y') as string) || 0
-        };
-      }
-      
-    private isBetween(value: number, min: number, length: number) {
-        return min - AXIS_EXTRA_RANGE < value && value < (min + length) + AXIS_EXTRA_RANGE;
-    }
-    
-    private getDistance(value1: number, value2: number) {
-        return Math.abs(value1 - value2);
-    }
-
-    private getSnapCoords(element: HTMLElement, axis?: string) {
-        const result: {
-            isOK: boolean;
-            offX: number;
-            offY: number;
-            x: number;
-            y: number;
-        } = {
-          isOK: false,
-          x: 0,
-          y: 0,
-          offX: 0,
-          offY: 0
-        };
-        
-        const currentElement = this.currentElement;
-
-        if (currentElement && currentElement !== element) {
-          const pos = this.getPosition(element);
-          const cur = this. getPosition(currentElement);
-          const distX1a = this.getDistance(pos.x, cur.x);
-          const distX1b = this.getDistance(pos.x, cur.x + currentElement.offsetWidth);
-          const distX2a = this.getDistance(pos.x + element.offsetWidth, cur.x);
-          const distX2b = this.getDistance(pos.x + element.offsetWidth, cur.x + currentElement.offsetWidth);
-          const distY1a = this.getDistance(pos.y, cur.y);
-          const distY1b = this.getDistance(pos.y, cur.y + currentElement.offsetHeight);
-          const distY2a = this.getDistance(pos.y + element.offsetHeight, cur.y);
-          const distY2b = this.getDistance(pos.y + element.offsetHeight, cur.y + currentElement.offsetHeight);
-          const distXa = Math.min(distX1a, distX2a);
-          const distXb = Math.min(distX1b, distX2b);
-          const distYa = Math.min(distY1a, distY2a);
-          const distYb = Math.min(distY1b, distY2b);
-          if (distXa < distXb) {
-            result.offX = this.offX1;
-          } else {
-            result.offX = this.offX2
-          }
-          if (distYa < distYb) {
-            result.offY = this.offY1;
-          } else {
-            result.offY = this.offY2
-          }
-          const distX1 = Math.min(distX1a, distX1b);
-          const distX2 = Math.min(distX2a, distX2b);
-          const distY1 = Math.min(distY1a, distY1b);
-          const distY2 = Math.min(distY2a, distY2b);
-          const distX = Math.min(distX1, distX2);
-          const distY = Math.min(distY1, distY2);
-          const dist = Math.max(distX, distY);
-          const acceptAxis = dist > CORNER_EXCLUDE_AXIS;
-      
-          result.x = distX1 < distX2 ? pos.x : pos.x + element.offsetWidth;
-          result.y = distY1 < distY2 ? pos.y : pos.y + element.offsetHeight;
-      
-          const inRangeX1 = this.isBetween(pos.x, cur.x, currentElement.offsetWidth);
-          const inRangeX2 = this.isBetween(cur.x, pos.x, element.offsetWidth);
-          const inRangeY1 = this.isBetween(pos.y, cur.y, currentElement.offsetHeight);
-          const inRangeY2 = this.isBetween(cur.y, pos.y, element.offsetHeight);
-      
-          switch (axis) {
-            case "x":
-              result.isOK = acceptAxis && (inRangeY1 || inRangeY2);
-              break;
-            case "y":
-              result.isOK = acceptAxis && (inRangeX1 || inRangeX2);
-              break;
-            default:
-              result.isOK = true;
-              break;
-          }
-        }
-        return result;
-      }
 }
